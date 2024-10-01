c4b573f9e68883ee2aa876025dc51738e59c8c9a
Lerian Febriana <kanglerian@gmail.com>
2024-10-01 10:38:38 +0700

    DB

--- config/config.js
+++ config/config.js
@@ -1,10 +1,21 @@
-{
+require('dotenv').config();
+const {
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_HOST,
+  DB_DIALECT,
+  DB_PORT
+} = process.env;
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "lp3itasikmysql",
-    "database": "dbo_otak",
-    "host": "103.163.111.39",
-    "dialect": "mysql"
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOST,
+    "dialect": DB_DIALECT,
+    "port": DB_PORT,
   },
   "test": {
     "username": "root",
